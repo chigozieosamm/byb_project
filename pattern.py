e8a2135a84012517388453226f0e566e8bad7acb
chigozieosamm <chigozieo@hotmail.co.uk>
2024-04-01 21:04:08 +0100

    Added files from task 4

---
+++ pattern.py
@@ -0,0 +1,11 @@
+star = "*"
+for i in range(1, 9):
+    if i <= 5:
+        print(star)
+        star = star + "*"
+    else:
+        print(star)
+        star = star[:-1]
+print("*")
+
+# CO24010013890 Chigozie Osammor
